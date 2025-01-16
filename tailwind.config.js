b10aa9ce224d0d209460e8353dac812deaaff0bf
Biokes <106444765+Biokes@users.noreply.github.com>
2025-01-16 17:41:07 +0100

    update global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/**/*.tsx","./app/**/*.tsx","./app/*.tsx"],
+  presets:[require("nativewind/preset")],
   theme: {
     extend: {},
   },
